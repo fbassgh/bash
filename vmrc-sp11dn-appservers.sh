4e2a53435471b11423c8e7bb87a2b66a6f3b367f
fbassgh <fbass79@gmail.com>
2017-02-08 14:36:43 -0700

    updated to take the vCenter host as a parameter.

--- vmrc-sp11dn-appservers.sh
+++ vmrc-sp11dn-appservers.sh
@@ -5,17 +5,18 @@
 
 # $1 = username
 # $2 = password
+# $3 = vcenter hostname
 usage()
 {
-    echo -e "Illegal number of parameters. You must specify your username and password."
+    echo -e "Illegal number of parameters. You must specify username, password, and vCenter hostname."
     exit 1
 }
 
-if [ "$#" -ne 2 ]; then
+if [ "$#" -ne 3 ]; then
     usage
 fi
 
-nohup sh vmrc -M vm-15099 -U $1 -P $2 -H jmsmgmtvctr.space.issinc.com &>/dev/null &
-nohup sh vmrc -M vm-15100 -U $1 -P $2 -H jmsmgmtvctr.space.issinc.com &>/dev/null &
+nohup sh vmrc -M vm-15099 -U $1 -P $2 -H $3 &>/dev/null &
+nohup sh vmrc -M vm-15100 -U $1 -P $2 -H $3 &>/dev/null &
 
 exit 0
